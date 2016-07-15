ce6543f8e9bf65f710382c61623da6981bcf998a
Ben Steinberg <benjamin_steinberg@harvard.edu>
2016-07-15 18:53:47 -0400

    Lose on_stop in Aktor for now

--- nodes/actors.py
+++ nodes/actors.py
@@ -15,17 +15,6 @@ class Aktor(pykka.ThreadingActor):
     def on_start(self):
         logger.info("Started actor %s" % (self.actor_urn,))
         
-    def on_stop(self):
-        """Tell Django so we can update the DB"""
-        try:
-            for node in Node.objects.filter(actor_urn=self.actor_urn):
-                logger.info("Stopping actor, updating node %s" % (node.name,))
-                node.actor_urn = None
-                node.started = False
-                node.save()
-        except:
-            pass
-
     def yell(self, content):
         logger.info('%s is yelling "%s"' % (self.actor_urn, content))
         message = { 'sender': self.actor_urn, 'data': content }
