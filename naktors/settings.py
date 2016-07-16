6cc430274c9eff92ee629b16c2cd0b165148226d
Ben Steinberg <benjamin_steinberg@harvard.edu>
2016-07-16 10:37:06 -0400

    Log Pykka debug messages

--- naktors/settings.py
+++ naktors/settings.py
@@ -164,6 +164,10 @@ LOGGING = {
             'handlers': ['console'],
             'level': 'INFO'
         },
+        'pykka': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        },
     }
 }
 
