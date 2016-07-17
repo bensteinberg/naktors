d72d48428acc77440d3dce5e67ef0e0b63488917
Ben Steinberg <benjamin_steinberg@harvard.edu>
2016-07-17 09:17:15 -0400

    Remove actorurls

--- naktors/urls.py
+++ naktors/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^nodes/', include('nodes.urls')),
-    url(r'^actors/', include('nodes.actorurls')),
     url(r'^admin/', admin.site.urls),
     url(r'^', include('django.contrib.auth.urls')),
 ]
